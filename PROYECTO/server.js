f52708d59a3a29462d18aa097cfd4f809e7c668f
PARG1703 <pabloaldo1703@gmail.com>
2026-05-26 20:14:15 -0600

    Configurar variables de entorno para la base de datos

--- PROYECTO/server.js
+++ PROYECTO/server.js
@@ -34,10 +34,10 @@ app.use(express.static(__dirname));
 
 // Conexión a la base de datos usando un Pool (más robusto)
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '', 
-    database: 'tech_shop_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
