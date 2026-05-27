5a7a9f966f709b639cc71fdcc9ccb2f7c54d6886
PARG1703 <pabloaldo1703@gmail.com>
2026-05-27 00:36:11 -0600

    Refactor: Hacer más estricta la configuración del puerto de BD

--- PROYECTO/server.js
+++ PROYECTO/server.js
@@ -38,7 +38,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 3000,
+    port: process.env.DB_PORT,
     ssl: {
         ca: fs.readFileSync(path.join(__dirname, 'isrgrootx1.pem'))
     },
